66b99a5702ebab5f89e5b41ebbb386eb525424b0
TranTungDuong1611 <tungduong123q@gmail.com>
2025-09-10 01:51:59 +0700

    Update: new layout for pages

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import NewWebsite from "./pages/MainPage.tsx";
 import ClassificationSummationPage from "./pages/ClassificationSummationPage.tsx";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
+      <Header />
+      
+      {/* Nội dung chính sẽ chiếm hết khoảng trống */}
       <main className="flex-grow">{children}</main>
+      
       <Footer />
     </div>
   );
 };
+
 
 const App = () => {
   return (
